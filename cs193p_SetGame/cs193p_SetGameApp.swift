f782ef9812ddc863ef2cfaa4d2fa178ffefc7f45
LGiki <qq547221432@gmail.com>
2025-02-28 13:36:05 +0800

    Initial Commit

---
+++ cs193p_SetGame/cs193p_SetGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  cs193p_SetGameApp.swift
+//  cs193p_SetGame
+//
+//  Created by LGiki on 2025/2/28.
+//
+
+import SwiftUI
+
+@main
+struct cs193p_SetGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
